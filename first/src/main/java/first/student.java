ac85cf333c8ae5933135e12f5db4a75ff8d3172b
sanket2000sonavane <sanket2000sonavane@gmail.com>
2021-09-17 16:21:31 +0530

    second commit

---
+++ first/src/main/java/first/student.java
@@ -0,0 +1,10 @@
+package first;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello from student");
+	}
+
+}
