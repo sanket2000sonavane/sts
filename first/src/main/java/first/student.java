2be041133ff5af549384307d49072334909e7091
sanket2000sonavane <sanket2000sonavane@gmail.com>
2021-09-20 11:12:24 +0530

    show method

--- first/src/main/java/first/student.java
+++ first/src/main/java/first/student.java
@@ -5,8 +5,14 @@ public class student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hello from student");
+       show();
 	}
-    public static void classA() {
+   
+    	public static void show() {
+        	int[] arr= {1,2,3,4,5};
+        	for (int i=0;i<5;i++) {
+        		System.out.print(i);
+        	}
     	System.out.println("Hello from class A");  
     	}
 }
